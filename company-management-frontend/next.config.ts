1d311c2b05621a147cfa3857a5dad76556911650
Saiful <azadahmedofficial@gmail.com>
2025-10-16 17:11:05 +0600

    ts off

--- company-management-frontend/next.config.ts
+++ company-management-frontend/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  typescript: {
+    // !! WARN !!
+    // This allows your app to build even if it has TypeScript errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
